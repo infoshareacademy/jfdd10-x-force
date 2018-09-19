62b710caa0ba15f44aa4ded022533221f52e0aa8
Hubert <hubert_w@autograf.pl>
2018-09-19 16:11:41 +0200

    player ready

--- Game/x-game.js
+++ Game/x-game.js
@@ -3,7 +3,7 @@ var appContainer = document.querySelector('#app');
 var boardData = [
   '____________________________',
   '____________________________',
-  '____________________________',
+  '_p__________________________',
   '____________________________',
   '____________________________',
   '____________________________',
@@ -42,6 +42,64 @@ for (var j = 0; j < boardData.length; j += 1) {
     cell.classList.add('gridCell');
     cell.setAttribute('data-x', i)
     cell.setAttribute('data-y', j)
+    if (rowData[i] === 'p') {
+      cell.classList.add('player');
+    }
     row.appendChild(cell);
   }
 }
+
+var direction = ''
+
+window.addEventListener('keydown', function(event){
+  var key = event.code
+
+  if (key === 'ArrowUp') {
+    direction = 'up';
+  }
+  if (key === 'ArrowDown') {
+    direction = 'down';
+  }
+  if (key === 'ArrowRight') {
+    direction = 'right';
+  }
+  if (key === 'ArrowLeft') {
+    direction = 'left';
+  }
+  
+})
+
+var directions = {
+  left : function (player) {
+    return player.previousElementSibling;
+  },
+  right: function (player) {
+    return player.nextElementSibling;
+  },
+  up: function (player) {
+    var colIndex = Array.from(player.parentElement.children).indexOf(player);
+    return player.parentElement.previousElementSibling.children[colIndex];
+  },
+  down: function (player) {
+    var colIndex = Array.from(player.parentElement.children).indexOf(player);
+    return player.parentElement.nextElementSibling.children[colIndex];
+  }
+
+}
+
+setInterval (function(){
+  var player = document.querySelector('.player');
+  var target = (directions[direction] || function () {
+    return null 
+  }) (player)
+
+  if (target != null && 
+    !target.classList.contains('wall')
+    ) {
+      player.classList.remove('player');
+      target.classList.add('player');
+    }
+
+
+
+}, 16)
